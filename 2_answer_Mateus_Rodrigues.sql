febbe2c7a7bd954819f675134b94d766ec0bf6c2
Mateus Leal <86207546+Mateus-L@users.noreply.github.com>
2022-12-02 13:07:11 -0300

    Update 2_answer_Mateus_Rodrigues.sql

--- 2_answer_Mateus_Rodrigues.sql
+++ 2_answer_Mateus_Rodrigues.sql
@@ -14,9 +14,9 @@ SELECT
 	concat(a.first_name, ' ', a.last_name) AS ator
 # Retornar nome e sobrenome do ator concatenados
 FROM film f
-INNER JOIN alugados al			ON  f.film_id		=	al.filme	# JOIN com subquery criada acima com o WITH
+INNER JOIN alugados al			ON  	f.film_id		=	al.filme	# JOIN com subquery criada acima com o WITH
 INNER JOIN film_actor fa		ON	f.film_id		=	fa.film_id	# Tabela de relacionamento
-INNER JOIN actor a				ON	fa.actor_id		=	a.actor_id	# Informações gerais de atores
+INNER JOIN actor a			ON	fa.actor_id		=	a.actor_id	# Informações gerais de atores
 GROUP BY a.actor_id					# Agrupar por ator
-ORDER BY count(a.actor_id) DESC 	# Fazer a contagem e ordenar pelos atores que mais apareceram
-LIMIT 1								# Retornar apenas o ator que mais aparece
+ORDER BY count(a.actor_id) DESC 			# Fazer a contagem e ordenar pelos atores que mais apareceram
+LIMIT 1							# Retornar apenas o ator que mais aparece
